2a4d8a68616879726eb8d0776d32787fa898271f
John Ray <john@johnraymon.com>
2019-03-29 15:54:32 -0400

    remove duplicate argument

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ class ViewBasedClient {
     })
   }
 
-  getAllRecords(scene, view, filters, page, rows_per_page, sort_field, sort_order, filters) {
+  getAllRecords(scene, view, filters, page, rows_per_page, sort_field, sort_order) {
     if (scene && view) {
       const options = {
         url: `pages/scene_${scene}/views/view_${view}/records` + ((filters) ? '?filters=' + encodeURIComponent(JSON.stringify(filters)) : '') +
